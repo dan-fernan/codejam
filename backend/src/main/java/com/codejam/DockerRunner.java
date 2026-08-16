c3eae8b1574cc497e4a71feeb3316c00994865b6
Daniel Fernandez <danielfernandez.ny@gmail.com>
2026-08-16 14:38:58 -0400

    Changed folder structure, updated code to handle checked exceptions and modified incorrect method syntax.

--- backend/src/main/java/com/codejam/DockerRunner.java
+++ backend/src/main/java/com/codejam/DockerRunner.java
@@ -1,12 +1,14 @@
-package com.example.demo;
+package com.codejam;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 import java.util.concurrent.*;
 
 public class DockerRunner {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		String hostDir = System.getProperty("user.dir") + "/test-code";
 
 		List<String> command = List.of(
@@ -32,7 +34,7 @@ public class DockerRunner {
 
 		if (!finished) {
 			process.destroyForcibly();
-			executor.shutDownNow();
+			executor.shutdownNow();
 			System.out.println("Process timed out and was killed.");
 			return;
 		}
