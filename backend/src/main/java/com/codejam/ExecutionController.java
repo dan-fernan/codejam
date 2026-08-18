db69a4defe0fdacf6cfd3f0eb27404c13fdea364
Daniel Fernandez <danielfernandez.ny@gmail.com>
2026-08-18 00:29:18 -0400

    Added basic frontend functionality, can send code over network and run it, receives stdout and stderr back. Also added basic CORS policy, as cross-port requests were being denied.

--- backend/src/main/java/com/codejam/ExecutionController.java
+++ backend/src/main/java/com/codejam/ExecutionController.java
@@ -1,12 +1,14 @@
 package com.codejam;
 
 import com.codejam.DockerRunner.ExecutionResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class ExecutionController {
 
     private record ExecuteRequest(String code, String language) {}
